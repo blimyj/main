609aecf0f2e012189f9d0cfdddd9533e3ea45aae
blimyj <blimyj21@gmail.com>
2019-10-14 01:26:12 +0800

    Fix checkstyle errors

--- src/main/java/seedu/address/logic/Logic.java
+++ src/main/java/seedu/address/logic/Logic.java
@@ -2,9 +2,7 @@ package seedu.address.logic;
 
 import java.nio.file.Path;
 
-import javafx.collections.ObservableList;
 import seedu.address.commons.core.GuiSettings;
-import seedu.address.commons.core.item.Item;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
